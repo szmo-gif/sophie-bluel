b5c0165c53243b5adfb6946671276859d2841442
Szmoniewski Baptiste <151198403+szmo-gif@users.noreply.github.com>
2024-05-20 17:37:28 +0200

    mise en place du log in

---
+++ FrontEnd/login.js
@@ -0,0 +1,28 @@
+document.getElementById('loginForm').addEventListener('submit', async function (event) {
+  event.preventDefault();
+
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  try {
+    const response = await fetch('http://localhost:5678/api/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email, password })
+    });
+
+    const result = await response.json();
+
+    if (!response.ok) {
+      document.getElementById('errorMessage').textContent = result.message || 'Erreur de connexion';
+    } else {
+      localStorage.setItem('token', result.token);
+      window.location.href = 'index.html';
+    }
+  } catch (error) {
+    console.error('Erreur:', error);
+    document.getElementById('errorMessage').textContent = 'Une erreur est survenue. Veuillez réessayer.';
+  }
+});
